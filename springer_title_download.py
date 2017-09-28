568719274293b238b0b5bfcc1aeadc6ed7a6ee23
Gerry <bibliodrone@users.noreply.github.com>
2017-09-28 17:11:13 -0400

    updated with comment

--- springer_title_download.py
+++ springer_title_download.py
@@ -1,3 +1,4 @@
+# partial sketch; doesn't really work as yet.
 from selenium import webdriver
 browser = webdriver.Firefox(executable_path='C:\Program Files\geckodriver.exe') # if system path edit doesn't work, use the 'executable_path' arg as here
 browser.get('http://www.springernature.com/gp/librarians/manage-your-account/marc-records/title-list-downloader')
